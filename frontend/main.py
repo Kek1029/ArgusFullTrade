8c7657011f1ea24d8f49d34cc0491c680f589ec8
Etders <topermaks1028r@gmail.com>
2025-08-12 20:45:05 +0300

    Initial commit

---
+++ frontend/main.py
@@ -0,0 +1,44 @@
+import logging
+
+import uvicorn
+from aiohttp.web_middlewares import middleware
+from fastapi import FastAPI
+from pydantic import BaseModel
+from env_config import get_config
+import httpx
+
+config = get_config()
+PORT = config["FASTAPI_PORT"]
+
+app = FastAPI()
+
+import os
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(filename="logs/frontend.log", filemode="w", level=logging.INFO, format="%(asctime)s %(message)s)")
+
+class RequestData(BaseModel):
+    to: int
+    data: dict
+
+@app.post("/route")
+async def route(req: RequestData):
+    logging.info(f"Received request: to: {req.to}, data: {req.data}")
+
+    try:
+        async with httpx.AsyncClient() as client:
+            middleware_port = config["MIDDLEWARE_PORT"]
+            middleware_url = f"http://middleware:{middleware_port}/route"
+            response = await client.post(url=middleware_url, json=req.data)
+            logging.info(f"Response status: {response.status_code}, body: {response.text}")
+            return {
+                "status": "OK",
+                "middleware_response": response.json()
+            }
+    except Exception as e:
+        logging.error(f"Exception: {e}")
+        return {"status": "ERROR", "detail": str(e)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
---
+++ middleware/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/route", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message": "Hello World"}`))
+	})
+	port, exists := os.LookupEnv("MIDDLEWARE_PORT")
+	if !exists {
+		port = "6132"
+	}
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), r)
+	if err != nil {
+		panic(err)
+	}
+}
